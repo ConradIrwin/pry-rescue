10a79bab63cc2bb024a90393eb0d3877d9ccd0cf
John Mair <jrmair@gmail.com>
2012-08-12 12:41:02 +1200

    Fix `_ex_` and ensure `_raised_` is properly set.

--- lib/pry-capture.rb
+++ lib/pry-capture.rb
@@ -28,41 +28,42 @@ class Pry
     ensure
       if raised.any?
         exception, bindings = raised.last
-        enter_exception_context(exception, bindings, raised)
+       enter_exception_context(exception, bindings, raised)
       end
     end
-
 
     # Start a Pry session in the context of the exception.
     # @param [Exception] exception The exception.
     # @param [Array<Binding>] bindings The call stack.
     def enter_exception_context(exception, bindings, raised)
       prune_call_stack!(bindings)
-
-      inject_local("_ex_", exception, bindings.first)
-      inject_local("_raised_", raised, bindings.first)
-
       if defined?(PryStackExplorer)
-        pry :call_stack => bindings
+        pry :call_stack => bindings, :hooks => pry_hooks(exception, raised)
       else
-        bindings.first.pry
+        bindings.first.pry :hooks => pry_hooks(exception, raised)
       end
     end
 
     private
 
+    # Define the :before_session hook for the Pry instance.
+    # This ensures that the `_ex_` and `_raised_` sticky locals are
+    # properly set.
+    def pry_hooks(ex, raised)
+      hooks = Pry.config.hooks.dup
+      hooks.add_hook(:before_session, :save_captured_exception) do |_, _, _pry_|
+        _pry_.last_exception = ex
+        _pry_.backtrace = ex.backtrace
+        _pry_.sticky_locals.merge!({ :_raised_ => raised })
+      end
+
+      hooks
+    end
+
     # Sanitize the call stack.
     # @param [Array<Binding>] bindings The call stack.
     def prune_call_stack!(bindings)
       bindings.delete_if { |b| [Pry, Interception].include?(b.eval("self")) }
-    end
-
-    # Inject a local variable into a binding.
-    def inject_local(var, object, binding)
-      Thread.current[:__intercept_var__] = object
-      binding.eval("#{var} = Thread.current[:__intercept_var__]")
-    ensure
-      Thread.current[:__intercept_var__] = nil
     end
   end
 end
