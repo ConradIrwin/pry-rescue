03b709e183261d3b2cd1db66444c58e9f498ac9f
Conrad Irwin <conrad.irwin@gmail.com>
2014-02-14 23:15:49 -0800

    Small tweaks
    Don't define a global run_exit_callbacks method, remove warnings.

--- lib/pry-rescue/kernel_exit_hooks.rb
+++ lib/pry-rescue/kernel_exit_hooks.rb
@@ -1,16 +1,20 @@
-Kernel.class_eval '@@exit_callbacks = []'
-
-Kernel.at_exit { Kernel.run_exit_callbacks }
-
-module Kernel
-  def at_exit(&block)
-    @@exit_callbacks.push block
+class << PryRescue
+  def exit_callbacks
+    @exit_callbacks ||= []
   end
 
   def run_exit_callbacks
     Pry::rescue do
-      @@exit_callbacks.dup.each &:call
+      exit_callbacks.dup.each(&:call)
     end
-    TOPLEVEL_BINDING.pry unless PryRescue.any_exception_captured
+    TOPLEVEL_BINDING.pry unless any_exception_captured
+  end
+end
+
+Kernel.at_exit { PryRescue.run_exit_callbacks }
+
+module Kernel
+  def at_exit(&block)
+    PryRescue.exit_callbacks.push block
   end
 end
