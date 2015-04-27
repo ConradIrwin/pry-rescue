c969ff74d1282ad2b8be5a64dcd776480adc6f88
Kareem Gan <kareemgan@gmail.com>
2015-04-26 22:11:18 -0500

    Updated to use the RSpec 3.2 syntax. Also grouped test using contexts.

--- spec/core_ext_spec.rb
+++ spec/core_ext_spec.rb
@@ -1,41 +1,44 @@
-require File.expand_path('../../lib/pry-rescue.rb', __FILE__)
+require 'spec_helper'
 
-describe 'Pry.rescue' do
-  it 'should call PryRescue.enter_exception_context' do
-    lambda{
-      PryRescue.should_receive(:enter_exception_context).once
+RSpec.describe '#Pry.rescue' do
+  it 'expect to call PryRescue.enter_exception_context' do
+    expect {
+      expect(PryRescue).to receive(:enter_exception_context).once
       Pry::rescue{ raise "foobar" }
-    }.should raise_error(/foobar/)
+    }.to raise_error(/foobar/)
   end
 
-  it "should retry on try-again" do
-    @called = 0
-    PryRescue.should_receive(:enter_exception_context).once{ throw :try_again }
+  it "expect to retry on try-again" do
+    called = 0
+    expect(PryRescue).to receive(:enter_exception_context).once{ throw :try_again }
     Pry::rescue do
-      @called += 1
-      raise "foobar" if @called == 1
+      called += 1
+      raise "foobar" if called == 1
     end
-    @called.should == 2
+    expect(called).to eq(2)
   end
 
-  it "should try-again from innermost block" do
-    @outer = @inner = 0
-    PryRescue.should_receive(:enter_exception_context).once{ throw :try_again }
+  it "expect to try-again from innermost block" do
+    outer = inner = 0
+    expect(PryRescue).to receive(:enter_exception_context).once{ throw :try_again }
     Pry::rescue do
-      @outer += 1
+      outer += 1
       Pry::rescue do
-        @inner += 1
-        raise "oops" if @inner == 1
+        inner += 1
+        raise "oops" if inner == 1
       end
     end
 
-    @outer.should == 1
-    @inner.should == 2
+    expect(outer).to eq(1)
+    expect(inner).to eq(2)
   end
 
-  it "should enter the first occurence of an exception that is re-raised" do
-    PryRescue.should_receive(:enter_exception_context).once{ |raised| raised.size.should == 1 }
-    lambda do
+  it "expect to enter the first occurence of an exception that is re-raised" do
+    expect(PryRescue).to receive(:enter_exception_context) do |raised|
+      expect(raised.size).to eq(1)
+    end
+
+    expect do
       Pry::rescue do
         begin
           raise "first_occurance"
@@ -43,55 +46,56 @@ describe 'Pry.rescue' do
           raise
         end
       end
-    end.should raise_error(/first_occurance/)
+    end.to raise_error(/first_occurance/)
   end
 
-  it "should not catch SystemExit" do
-    PryRescue.should_not_receive(:enter_exception_context)
+  it "expect to not catch SystemExit" do
+    expect(PryRescue).to_not receive(:enter_exception_context)
 
-    lambda do
+    expect do
       Pry::rescue do
         exit
       end
-    end.should raise_error SystemExit
+    end.to raise_error SystemExit
   end
 
-  it 'should not catch Ctrl+C' do
-    PryRescue.should_not_receive(:enter_exception_context)
-    lambda do
+  it 'expect to not catch Ctrl+C' do
+    expect(PryRescue).to_not receive(:enter_exception_context)
+
+    expect do
       Pry::rescue do
         raise Interrupt, "ctrl+c (fake)"
       end
-    end.should raise_error Interrupt
+    end.to raise_error Interrupt
   end
 end
 
-describe "Pry.rescued" do
+RSpec.describe "Pry.rescued" do
 
-  it "should raise an error if used outwith Pry::rescue" do
+  it "expect to raise an error if used outwith Pry::rescue" do
     begin
       raise "foo"
     rescue => e
-      Pry.should_receive(:warn)
+      expect(Pry).to receive(:warn)
       Pry.rescued(e)
     end
   end
 
-  it "should raise an error if used on an exception not raised" do
+  it "expect to raise an error if used on an exception not raised" do
     Pry::rescue do
-      Pry.should_receive(:warn) do |message|
-        message.should =~ /^WARNING: Tried to inspect exception outside of Pry::rescue/
+      expect(Pry).to receive(:warn) do |message|
+        expect(message).to match(/^WARNING: Tried to inspect exception outside of Pry::rescue/)
       end
       Pry.rescued(RuntimeError.new("foo").exception)
     end
   end
 
-  it "should call Pry.enter_exception_context" do
+  it "expect to call Pry.enter_exception_context" do
     Pry::rescue do
       begin
         raise "foo"
       rescue => e
-        PryRescue.should_receive(:enter_exception_context).once
+        expect(PryRescue).to receive(:enter_exception_context)
         Pry::rescued(e)
       end
     end
--- spec/peek_spec.rb
+++ spec/peek_spec.rb
@@ -1,18 +1,19 @@
+require 'spec_helper'
+
 ENV['NO_PEEK_STARTUP_MESSAGE'] = 'true'
 
-describe "PryRescue.peek!" do
-  it "should open a pry in the binding of caller" do
+RSpec.describe "PryRescue.peek!" do
+  it "expect to open a pry in the binding of caller" do
     Pry.config.input = StringIO.new("foo = 6\nexit\n")
     Pry.config.output = StringIO.new
     foo = 5
 
-    lambda do
+    expect do
       PryRescue.peek!
-    end.should change{ foo }.from(5).to(6)
+    end.to change{ foo }.from(5).to(6)
   end
 
-  # this will fail, or not?
-  it 'should include the entire call stack' do
+  it 'expect to include the entire call stack' do
     Pry.config.input = StringIO.new("up\nfoo = 6\nexit\n")
     Pry.config.output = StringIO.new
 
@@ -22,8 +23,8 @@ describe "PryRescue.peek!" do
 
     foo = 5
 
-    lambda do
+    expect do
       PryRescue.peek!
-    end.should change{ foo }.from(5).to(6)
+    end.to change{ foo }.from(5).to(6)
   end
 end
