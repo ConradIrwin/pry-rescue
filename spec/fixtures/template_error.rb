7122ac705cc0086e69ee32404a73ed066453e44b
Eloy <ej.perezgomez@gmail.com>
2017-05-08 13:32:06 +0200

    Add missing comment

--- spec/fixtures/template_error.rb
+++ spec/fixtures/template_error.rb
@@ -1,3 +1,8 @@
+# This Exception Class behaves same as ActionView::Template::Error
+# https://github.com/rails/rails/blob/master/actionview/lib/action_view/template/error.rb#L68
+#
+# It encapsulates an Exception and stores the latest Exception raised in its
+# `cause` attribute
 class TemplateError < StandardError
   attr_reader :cause
 
